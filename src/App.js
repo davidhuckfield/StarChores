d83a59cc7a47f2a84aa90dd2c9b3f2008bcd1cd1
davidhuckfield <djhuckfield@hotmail.co.uk>
2023-08-12 16:48:39 +0100

    Fonts installed in CSS, tested and all but one (outline) working. No idea why outline not working

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <p>This will be the Star Chores Website</p>
+      <p className="hollow">This is the hollow font</p>
+      <p className="standard">This is the standard font</p>
+      <p className="rounded">This is the rounded font</p>
+      <p className="outline">This is the outline font</p>
       
     </div>
   );
