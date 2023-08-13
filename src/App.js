420026f79411c5310d3ee390b54844d88651ae84
davidhuckfield <djhuckfield@hotmail.co.uk>
2023-08-13 16:21:55 +0100

    Added SVG logo file and linked in App.js, not displaying

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+
+      <img className="logo" src="./images/Final_Logo.svg" alt="Star Chores Logo" />
       <p className="hollow">This is the hollow font</p>
       <p className="standard">This is the standard font</p>
       <p className="rounded">This is the rounded font</p>
       <p className="outline">This is the outline font</p>
+      <p className="testing">TESTING</p>
       
     </div>
   );
