b54c917d78dd7af1900551fe2afbcb3d54c33e5d
davidhuckfield <djhuckfield@hotmail.co.uk>
2023-08-13 19:29:56 +0100

    SVG logos added for comparison, last font now working for some inexplicable reason

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import FinalLogo from "./images/Final_Logo.svg"
+import TestLogo from "./images/Test_Logo.svg"
 
 function App() {
   return (
     <div className="App">
 
-      <img className="logo" src="./images/Final_Logo.svg" alt="Star Chores Logo" />
+      <img className="logo" src={FinalLogo} alt="Star Chores Logo" />
+      <img className="logo" src={TestLogo} alt="Star Chores Logo" />
+
       <p className="hollow">This is the hollow font</p>
       <p className="standard">This is the standard font</p>
       <p className="rounded">This is the rounded font</p>
