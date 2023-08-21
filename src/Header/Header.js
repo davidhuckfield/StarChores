1714c629ab58b462a7fcf06bb8d1d3a8d82824d5
davidhuckfield <djhuckfield@hotmail.co.uk>
2023-08-21 15:40:28 +0100

    Converted navbar text to SVG graphics to avoid font installation/compatibility issues

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -3,6 +3,10 @@ import { Link } from "react-router-dom";
 import './Header.css';
 import TextLogo from "../images/Text_Logo.svg"
 import ImageLogo from "../images/Image_Logo.png"
+import About from "../images/about.svg"
+import Contact from "../images/contact.svg"
+import Services from "../images/services.svg"
+import FAQ from "../images/faq.svg"
 
 function Header() {
   return (
@@ -12,10 +16,10 @@ function Header() {
         <img className="textlogo" src={TextLogo} alt="Star Chores Logo Text" />
       </div>
       <ul className="navbar">
-        <li><Link to="/about">about</Link></li>
-        <li><Link to="/contact">contact</Link></li>
-        <li><Link to="/services">services</Link></li>
-        <li><Link to="/faq">faq</Link></li>
+        <li><Link to="/about"><img className="about" src={About} alt="About Us" /></Link></li>
+        <li><Link to="/contact"><img className="contact" src={Contact} alt="Contact Us" /></Link></li>
+        <li><Link to="/services"><img className="services" src={Services} alt="Our Services" /></Link></li>
+        <li><Link to="/faq"><img className="faq" src={FAQ} alt="Frequently Asked Questions" /></Link></li>
       </ul>
 
       </header>
