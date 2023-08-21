b2c7c8e1da9a5f40a11fa732310e659e8c85c596
davidhuckfield <djhuckfield@hotmail.co.uk>
2023-08-21 16:07:06 +0100

    Further changes to SVG and CSS, unable to set SVG fill while importing as img

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,9 +1,11 @@
 import './Home.css';
+import TestIcon from "../images/aboutcopy.svg"
 
 function Home() {
   return (
     <div className="Home">
         <p>This is the home page</p>
+        <img className="testicon" src={TestIcon} alt="Test Icon" />
     </div>
   );
 }
