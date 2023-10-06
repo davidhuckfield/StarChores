670582b47abe4238f2913e89f67d225a997e5fb2
davidhuckfield <djhuckfield@hotmail.co.uk>
2023-10-06 18:38:48 +0100

    Dummy text and form added to Contact page

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -3,7 +3,21 @@ import './Contact.css';
 function Contact() {
   return (
     <div className="Contact">
-        <p>This is the Contact page</p>
+      <div id="contactdetails">
+        <h1>CONTACT US</h1>
+        <p>Phone: 555-555-5555</p>
+        <p>Email:enquiries@starchores.co.uk</p>
+        <p>Address: 123 Skywalker Street, Tattooine, SW1 1AA</p>
+      </div>
+
+      <div id="contactform">
+        <h1>SEND US A MESSAGE</h1>
+        <input type="text" placeholder="Name" />
+        <input type="text" placeholder="Email" />
+        <input type="text" placeholder="Message" id="message" />
+        <button>SUBMIT</button>
+      </div>
+
     </div>
   );
 }
