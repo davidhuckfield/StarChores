1b9a298a8a7cb254ad0c803f3bb349105933df5e
davidhuckfield <djhuckfield@hotmail.co.uk>
2023-10-12 15:44:53 +0100

    Added phone link to phone number in footer for mobile devices

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -4,15 +4,16 @@ import AtSymbol from "../images/at_symbol.svg"
 function Footer() {
   return (
     <div className="Footer">
-
-    <p className="standard">07929632287</p>
-    <a href="mailto:enquiries@starchores.co.uk">
-    <div className="emailaddress">
-      <p className="standard">enquiries</p>
-      <img src={AtSymbol} alt="at symbol"/>
-      <p className="standard">starchores.co.uk</p>
-    </div>
-    </a>
+      <a href="tel:+447929632287">
+      <p className="standard">07929632287</p>
+      </a>
+      <a href="mailto:enquiries@starchores.co.uk">
+        <div className="emailaddress">
+          <p className="standard">enquiries</p>
+          <img src={AtSymbol} alt="at symbol"/>
+          <p className="standard">starchores.co.uk</p>
+        </div>
+      </a>
     </div>
   );
 }
