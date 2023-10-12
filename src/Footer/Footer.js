99bf0af630fec0ec1bc99d3301348ab51ec4beee
davidhuckfield <djhuckfield@hotmail.co.uk>
2023-10-12 15:41:41 +0100

    Added email link and changed sizing of email text to be more dynamic

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -6,11 +6,13 @@ function Footer() {
     <div className="Footer">
 
     <p className="standard">07929632287</p>
+    <a href="mailto:enquiries@starchores.co.uk">
     <div className="emailaddress">
       <p className="standard">enquiries</p>
       <img src={AtSymbol} alt="at symbol"/>
       <p className="standard">starchores.co.uk</p>
     </div>
+    </a>
     </div>
   );
 }
