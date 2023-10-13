91cfa800a2ac0b2b0c8fa26ab5c6c9cc41d3a6c0
davidhuckfield <djhuckfield@hotmail.co.uk>
2023-10-13 18:53:07 +0100

    Added some dummy text to Services page

--- src/Services/Services.js
+++ src/Services/Services.js
@@ -3,7 +3,20 @@ import './Services.css';
 function Services() {
   return (
     <div className="Services">
-        <p>This is the Services page</p>
+        <h1>Our services include</h1>
+          <ul id="serviceslist">
+            <li>Window Cleaning</li>
+            <li>Traditional & Water fed pole Methods</li>
+            <li>Conservatory Cleaning</li>
+            <li>Power Washing driveways & patios</li>
+            <li>Gutter, Fascia & Soffit Clean</li>
+            <li>Moss Removal Soft Wash</li>
+            <li>Gutter Repair or Replace</li>
+          </ul>
+            <div className="servicestext">
+              <p>In order to help us to get the best result our cleaning technicians use the latest equipment, technology and products.</p>
+              <p>This means that our customers get great results every time.</p>
+            </div>
     </div>
   );
 }
