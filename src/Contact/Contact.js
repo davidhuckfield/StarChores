eb1fb435210e3b85795aa25e5c1c9fe0c07052a4
davidhuckfield <djhuckfield@hotmail.co.uk>
2023-10-20 15:27:11 +0100

    Repushing to enable form detection

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -5,7 +5,7 @@ function Contact() {
     <div className="Contact">
       <div id="contactdetails">
         <h1>CONTACT US</h1>
-        <p>Phone: 555-555-5555</p>
+        <p>Phone: 555-555-5556</p>
         <p>Email:enquiries@starchores.co.uk</p>
         <p>Address: 123 Skywalker Street, Tattooine, SW1 1AA</p>
       </div>
