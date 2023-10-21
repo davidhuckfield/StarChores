8943593791cb95fd6a508cb4140086ea4f93b75a
davidhuckfield <djhuckfield@hotmail.co.uk>
2023-10-21 19:07:21 +0100

    Removed RECAPTCHA code to see if it starts form working again

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -10,7 +10,7 @@ function Contact() {
         <p>Address: 123 Skywalker Street, Tattooine, SW1 1AA</p>
       </div>
 
-      <form name="contact" method="POST" id="contact" data-netlify-recaptcha="true" data-netlify="true" >
+      <form name="contact" method="POST" id="contact" data-netlify="true" >
         <h1>SEND US A MESSAGE</h1>
         <input type="hidden" name="form-name" value="contact" />
         <label for="name">Name</label>
@@ -19,7 +19,6 @@ function Contact() {
         <input type="text" name="email" id="email" placeholder="Email" required/>
         <label for="message">Message</label>
         <input type="text" name="message" id="message" placeholder="Message" required/>
-        <div data-netlify-recaptcha="true"></div>
         <button type="submit">SUBMIT</button>
       </form>
 
