dd742ac524c8e60b20781696b5604a21ef5fe399
davidhuckfield <djhuckfield@hotmail.co.uk>
2023-10-21 19:01:51 +0100

    Hallelujah - form is working. Trying to add RECAPTCHA

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -10,7 +10,7 @@ function Contact() {
         <p>Address: 123 Skywalker Street, Tattooine, SW1 1AA</p>
       </div>
 
-      <form name="contact" method="POST" id="contact" data-netlify="true">
+      <form name="contact" method="POST" id="contact" data-netlify="true" data-netlify-recaptcha="true">
         <h1>SEND US A MESSAGE</h1>
         <input type="hidden" name="form-name" value="contact" />
         <label for="name">Name</label>
@@ -19,6 +19,7 @@ function Contact() {
         <input type="text" name="email" id="email" placeholder="Email" required/>
         <label for="message">Message</label>
         <input type="text" name="message" id="message" placeholder="Message" required/>
+        <div data-netlify-recaptcha="true"></div>
         <button type="submit">SUBMIT</button>
       </form>
 
