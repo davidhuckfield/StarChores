348f1a9139e8b73cbb77163586d22f73f70d878c
davidhuckfield <djhuckfield@hotmail.co.uk>
2023-10-22 16:20:52 +0100

    Set up email address and phone number as SVGs to avoid font issues and enable hover highlighting

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,18 +1,15 @@
 import './Footer.css';
-import AtSymbol from "../images/at_symbol.svg"
+import EmailAddress from "../NavButtons/EmailAddress"
+import PhoneNumber from '../NavButtons/PhoneNumber';
 
 function Footer() {
   return (
     <div className="Footer">
       <a href="tel:+447929632287">
-      <p className="standard">07929632287</p>
+      <PhoneNumber />
       </a>
       <a href="mailto:enquiries@starchores.co.uk">
-        <div className="emailaddress">
-          <p className="standard">enquiries</p>
-          <img src={AtSymbol} alt="at symbol"/>
-          <p className="standard">starchores.co.uk</p>
-        </div>
+      <EmailAddress />
       </a>
     </div>
   );
