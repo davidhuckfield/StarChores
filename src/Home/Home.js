a71156f498d513f0257d5cfdd3433a65356f43d3
davidhuckfield <djhuckfield@hotmail.co.uk>
2023-11-03 17:36:32 +0000

    Added placeholder to home page based on flyers

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -3,7 +3,16 @@ import './Home.css';
 function Home() {
   return (
     <div className="Home">
-        <p>This is the home page</p>
+        <p>EXTERIOR PROPERTY CLEANING</p>
+        <ul>
+          <li>Traditional Window Cleaning</li>
+          <li>Water-fed Pole Cleaning</li>
+          <li>Gutter Cleaning</li>
+          <li>Conservatory Cleaning</li>
+          <li>Fascia & Soffit Cleaning</li>
+          <li>Soft Washing</li>
+          <li>First Cleans & Maintenance</li>
+        </ul>
     </div>
   );
 }
