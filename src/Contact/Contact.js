3e760600514c9ca00449c2f2bda70d15e51ab058
davidhuckfield <djhuckfield@hotmail.co.uk>
2023-11-15 21:20:11 +0000

    Changed contact details

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -11,9 +11,8 @@ function Contact() {
     <div className="Contact">
       <div id="contactdetails">
         <h1>CONTACT US</h1>
-        <p>Phone: 555-555-5556</p>
-        <p>Email:enquiries@starchores.co.uk</p>
-        <p>Address: 123 Skywalker Street, Tattooine, SW1 1AA</p>
+        <p>Phone: 07929632287</p>
+        <p>Email:starchores@hotmail.com</p>
       </div>
 
       <h1>SEND US A MESSAGE</h1>
