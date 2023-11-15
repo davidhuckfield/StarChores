24bbf2d1c4dc21814c3f70b1b225ab3851e4ab67
davidhuckfield <djhuckfield@hotmail.co.uk>
2023-11-15 21:24:42 +0000

    Removed dummy text from About page

--- src/About/About.js
+++ src/About/About.js
@@ -11,32 +11,8 @@ function About() {
 
           <div className="AboutText bodytext">
             <h1>ABOUT US</h1>
-              <p id="p1">We are a well-established window cleaning company providing an exterior and interior window cleaning service to a high standard.</p>
-              <p>We specialise in pure water fed pole system cleaning methods, reaching heights of up to 20 metres from ground level with state of the art lightweight carbon fibre poles.</p>
-              <p>We are known for our safety-conscious approach, friendly operatives and competitive prices.</p>
-              <p>We have been providing a professional window cleaning service since the early 1990s to a variety of businesses and residents within the West Midlands and beyond.</p>
-              <p>Contact us today for a free no obligation quote!</p>
-              <p>Adding more text to test scrolling</p>
-              <p>Adding more text to test scrolling</p>
-              <p>Adding more text to test scrolling</p>
-              <p>Adding more text to test scrolling</p>
-              <p>Adding more text to test scrolling</p>
-              <p>Adding more text to test scrolling</p>
-              <p>Adding more text to test scrolling</p>
-              <p>Adding more text to test scrolling</p>
-              <p>Adding more text to test scrolling</p>
-              <p>Adding more text to test scrolling</p>
-              <p>Adding more text to test scrolling</p>
-              <p>Adding more text to test scrolling</p>
-              <p>Adding more text to test scrolling</p>
-              <p>Adding more text to test scrolling</p>
-              <p>Adding more text to test scrolling</p>
-              <p>Adding more text to test scrolling</p>
-              <p>Adding more text to test scrolling</p>
-              <p>Adding more text to test scrolling</p>
-              <p>Adding more text to test scrolling</p>
-              <p>Adding more text to test scrolling</p>
-              <p>Adding more text to test scrolling</p>
+              <p id="p1">Providing exterior cleaning services.</p>
+
           </div>
         </div>
     </div>
