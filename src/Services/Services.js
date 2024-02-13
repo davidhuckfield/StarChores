54286ad7bd8c08fc11d7afa42fe09e9fcfa7951b
davidhuckfield <djhuckfield@hotmail.co.uk>
2024-02-13 17:14:09 +0000

    Services component styling mostly done, dummy images for now

--- src/Services/Services.js
+++ src/Services/Services.js
@@ -1,4 +1,10 @@
 import './Services.css';
+import traditionalImage from "../images/dummy_images/traditional.jpg";
+import waterFedPoleImage from "../images/dummy_images/waterfedpole.jpg";
+import conservatoryImage from "../images/dummy_images/conservatory.jpg";
+import gutterImage from "../images/dummy_images/gutter.jpg";
+import fasciaSoffitImage from "../images/dummy_images/fasciasoffit.jpg";
+import restorationImage from "../images/dummy_images/restoration.jpg";
 
 function Services() {
   return (
@@ -6,38 +12,50 @@ function Services() {
 
         <div className="service">
         <h2>Traditional Window Cleaning</h2>
-        <p>Our traditional window cleaning service uses time-honored techniques to achieve crystal-clear windows. This method involves manually scrubbing the window glass with a squeegee and cleaning solution to remove dirt, grime, and streaks. Our skilled cleaners apply a precise amount of pressure and technique to ensure that each window is left spotless and streak-free. Ideal for accessible windows, this method allows for close-up attention to detail and the ability to tackle stubborn dirt effectively, ensuring your windows shine and enhance the overall appearance of your property.</p>
-        <img src="../images/dummy_images/traditional.jpg" alt="Traditional Window Cleaning"/>
-      </div>
+          <div className="content">
+          <p>Our traditional window cleaning service uses time-honored techniques to achieve crystal-clear windows. This method involves manually scrubbing the window glass with a squeegee and cleaning solution to remove dirt, grime, and streaks. Our skilled cleaners apply a precise amount of pressure and technique to ensure that each window is left spotless and streak-free. Ideal for accessible windows, this method allows for close-up attention to detail and the ability to tackle stubborn dirt effectively, ensuring your windows shine and enhance the overall appearance of your property.</p>
+          <img src={traditionalImage} alt="Traditional Window Cleaning"/>
+          </div>
+        </div>
 
       <div className="service">
         <h2>Water-fed Pole Window Cleaning</h2>
-        <p>Our water-fed pole window cleaning service utilizes advanced technology to clean hard-to-reach windows safely and efficiently. This method employs telescopic poles that can extend to clean windows at significant heights, using purified water pumped through the poles. The purified water, free from minerals and impurities, leaves the windows to dry naturally to a spotless finish without the need for squeegees or ladders. This method not only ensures your privacy and reduces disturbance but also is environmentally friendly and provides a thorough clean that maintains the integrity and longevity of your windows.</p>
-        <img src="../images/dummy_images/waterfedpole.jpg" alt="Water-fed Pole Window Cleaning"/>
+          <div className="content">
+          <p>Our water-fed pole window cleaning service utilizes advanced technology to clean hard-to-reach windows safely and efficiently. This method employs telescopic poles that can extend to clean windows at significant heights, using purified water pumped through the poles. The purified water, free from minerals and impurities, leaves the windows to dry naturally to a spotless finish without the need for squeegees or ladders. This method not only ensures your privacy and reduces disturbance but also is environmentally friendly and provides a thorough clean that maintains the integrity and longevity of your windows.</p>
+          <img src={waterFedPoleImage} alt="Water-fed Pole Window Cleaning"/>
+          </div>
       </div>
       
       <div className="service">
         <h2>Conservatory Cleaning</h2>
-        <p>Our conservatory cleaning service is designed to rejuvenate your conservatory, making it a bright and inviting space once again. We clean all parts of the conservatory, including the roof, windows, and frames, using gentle yet effective cleaning solutions to remove algae, moss, and dirt buildup. Our team pays special attention to detail, ensuring that even the most hard-to-reach areas are left spotless, restoring your conservatory's shine and transparency without causing any damage to its structure or materials.</p>
-        <img src="../images/dummy_images/conservatory.jpg" alt="Conservatory Cleaning"/>
+          <div className="content">
+          <p>Our conservatory cleaning service is designed to rejuvenate your conservatory, making it a bright and inviting space once again. We clean all parts of the conservatory, including the roof, windows, and frames, using gentle yet effective cleaning solutions to remove algae, moss, and dirt buildup. Our team pays special attention to detail, ensuring that even the most hard-to-reach areas are left spotless, restoring your conservatory's shine and transparency without causing any damage to its structure or materials.</p>
+          <img src={conservatoryImage} alt="Conservatory Cleaning"/>
+          </div>
       </div>
 
       <div className="service">
         <h2>Gutter Cleaning</h2>
-        <p>Gutter cleaning is essential for preventing water damage and maintaining the integrity of your property's roofing system. Our gutter cleaning service involves removing leaves, moss, and debris that can clog your gutters and downspouts, ensuring proper water flow and drainage. Using specialized tools and equipment, our professionals safely clear your gutters, conducting a thorough cleanup to protect your home from the potential damages caused by water overflow, such as foundation issues and basement flooding.</p>
-        <img src="../images/dummy_images/gutter.jpg" alt="Gutter Cleaning"/>
+          <div className="content">
+          <p>Gutter cleaning is essential for preventing water damage and maintaining the integrity of your property's roofing system. Our gutter cleaning service involves removing leaves, moss, and debris that can clog your gutters and downspouts, ensuring proper water flow and drainage. Using specialized tools and equipment, our professionals safely clear your gutters, conducting a thorough cleanup to protect your home from the potential damages caused by water overflow, such as foundation issues and basement flooding.</p>
+          <img src={gutterImage} alt="Gutter Cleaning"/>
+          </div>
       </div>
 
       <div className="service">
         <h2>Fascia & Soffit Cleaning</h2>
-        <p>The fascia and soffit are key elements of your roof structure, often overlooked but crucial for both aesthetics and protection. Our fascia and soffit cleaning service removes dirt, grime, and algae, preventing decay and extending their lifespan. We use gentle cleaning agents and soft brushes to meticulously clean these areas, enhancing your property's curb appeal while ensuring the longevity of your roofing components.</p>
-        <img src="../images/dummy_images/fasciasoffit.jpg" alt="Fascia & Soffit Cleaning"/>
+          <div className="content">
+          <p>The fascia and soffit are key elements of your roof structure, often overlooked but crucial for both aesthetics and protection. Our fascia and soffit cleaning service removes dirt, grime, and algae, preventing decay and extending their lifespan. We use gentle cleaning agents and soft brushes to meticulously clean these areas, enhancing your property's curb appeal while ensuring the longevity of your roofing components.</p>
+          <img src={fasciaSoffitImage} alt="Fascia & Soffit Cleaning"/>
+          </div>
       </div>
 
       <div className="service">
         <h2>Window Restoration Cleans</h2>
-        <p>Our window restoration cleaning service is specifically designed for windows that have suffered from prolonged exposure to elements, hard water stains, or oxidation. This service goes beyond standard cleaning, employing specialized cleaning solutions and techniques to restore the clarity and shine of your windows. We carefully remove stubborn stains and buildup without damaging the glass, revitalizing your windows to their original condition and improving the overall look of your property.</p>
-        <img src="../images/dummy_images/restoration.jpg" alt="Window Restoration Cleans"/>
+          <div className="content">
+          <p>Our window restoration cleaning service is specifically designed for windows that have suffered from prolonged exposure to elements, hard water stains, or oxidation. This service goes beyond standard cleaning, employing specialized cleaning solutions and techniques to restore the clarity and shine of your windows. We carefully remove stubborn stains and buildup without damaging the glass, revitalizing your windows to their original condition and improving the overall look of your property.</p>
+          <img src={restorationImage} alt="Window Restoration Cleans"/>
+          </div>
       </div>
     </div>
   );
